2915f4dca817295cee9b4326c93f73ac2b69c304
EnderLuca41 <lucavogel111@gmail.com>
2022-08-31 20:10:59 +0100

    made k and sha224_256() static

--- src/hashing/sha/sha224_256.c
+++ src/hashing/sha/sha224_256.c
@@ -3,7 +3,7 @@
 #include <util.h>
 #include <stdbool.h>
 
-const uint32_t k[] = {
+const const uint32_t k[] = {
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -80,7 +80,7 @@ static bool generate_chunk(uint8_t chunk[64], Sha256Buffer *buffer){
 }
 
 //Sha256 but you can specify the h constants, useful for sha224 so you not have to rewrite everyhing
-void sha224_256(uint8_t hash[32], const void* input, size_t len, const uint32_t Hs[8]){
+static void sha224_256(uint8_t hash[32], const void* input, size_t len, const uint32_t Hs[8]){
     Sha256Buffer buffer;
     buffer.currentLen = len;
     buffer.done = false;
