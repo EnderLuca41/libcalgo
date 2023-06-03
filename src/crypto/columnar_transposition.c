32e82084cb0e4a8da3e69f2769ef2f9de9cd4983
EnderLuca41 <lucavogel111@gmail.com>
2023-06-03 16:01:29 +0200

    Changed columnar transpostion cipher, so it does not pad with X's anymore

--- src/crypto/columnar_transposition.c
+++ src/crypto/columnar_transposition.c
@@ -5,11 +5,15 @@
 
 /*
  * Function assumes that keyword stores a 7bit big ascii character
- * Returns false if a character is repeated in keyword
+ * Returns false if a character is repeated in keyword or a allocation fails
  */
 bool columnar_transpostion_encrypt(const char *input, char *dest, const char *keyword){
+    if(*input == '\0') 
+        return true; //Empty string, no error or false input
+
     uint8_t keywordLen = 0;
 
+    //Loop trough the code, if a character is reapead break
     uint8_t i = 0;
     for(; keyword[i] != 0; i++){
         keywordLen++;
@@ -23,22 +27,25 @@ bool columnar_transpostion_encrypt(const char *input, char *dest, const char *ke
             break;
     }
 
-    //Return if a character is more than one time in keyword
+    //check if i reached the null terminator,
+    //if not a character is repeated
     if(keyword[i] != 0)
         return false;
 
     //Setup lookup table to get index of a character in keyword
-    char lookup[128];
-    memset(lookup, -1, 128);
+    char lookup[256];
+    memset(lookup, -1, 256);
     for(i = 0; i < keywordLen; i++){
         lookup[keyword[i]] = i;
     }
 
     uint8_t *alphabetOrder = malloc(keywordLen);
+    if(alphabetOrder == NULL)
+        return false;
     
-    //Fill alphabetOrder
+    //Fill with the alphabetic order position of each character in keyowrd
     uint8_t alphabetOrderPtr = 0;
-    for(i = 1; i < 128; i++){
+    for(i = 1; i != 0; i++){
         if(lookup[i] != -1){
             alphabetOrder[lookup[i]] = alphabetOrderPtr;
             alphabetOrderPtr++;
@@ -49,20 +56,23 @@ bool columnar_transpostion_encrypt(const char *input, char *dest, const char *ke
     uint32_t rows = inputLen / keywordLen + (inputLen % keywordLen != 0);
     uint8_t columns = keywordLen;
 
-    //Main encryption Loop
     for(i = 0; i < columns; i++){
-        for(uint32_t j = 0; j < rows; j++){
-            if(j * columns + i >= inputLen) {
-                //Pad with X's if the grid is bigger than input
-                dest[alphabetOrder[i] * rows + j] = 'X';
-                continue;
-            }
+        //Calculate how many columns have been shorter before,
+        //to subtract them later from the index
+        uint32_t shorterColumns = 0;
+        if(alphabetOrder[i] > inputLen % columns)
+            shorterColumns = alphabetOrder[i] - columns - shorterColumns + 1;
 
-            dest[alphabetOrder[i] * rows + j] = input[j * columns + i];
+        for(uint32_t j = 0; j < rows; j++){
+            if(j * columns + i >= inputLen)
+                continue;
+            
+            dest[alphabetOrder[i] * rows + j - shorterColumns] = input[j * columns + i];
         }
+
     }
 
-    dest[rows * keywordLen] = 0;
+    dest[inputLen] = '\0';
 
     free(alphabetOrder);
     return true;
@@ -74,17 +84,17 @@ void columnar_transposition_decrypt(const char *input, char *dest, const char *k
     uint8_t keywordLen = strlen(keyword);
 
     //Setup lookup table to get index of a character in keyword
-    char lookup[128];
-    memset(lookup, -1, 128);
+    char lookup[256];
+    memset(lookup, -1, 256);
     for(uint8_t i = 0; i < keywordLen; i++){
         lookup[keyword[i]] = i;
     }
 
     uint8_t *alphabetOrder = malloc(keywordLen);
     
-    //Fill alphabetOrder
+    //Fill with the alphabetic order position of each character in keyowrd
     uint8_t alphabetOrderPtr = 0;
-    for(uint8_t i = 1; i < 128; i++){
+    for(uint8_t i = 1; i != 0; i++){
         if(lookup[i] != -1){
             alphabetOrder[lookup[i]] = alphabetOrderPtr;
             alphabetOrderPtr++;
@@ -93,16 +103,27 @@ void columnar_transposition_decrypt(const char *input, char *dest, const char *k
 
 
     size_t inputLen = strlen(input);
+    uint32_t rows = inputLen / keywordLen + (inputLen % keywordLen != 0);
     uint8_t columns = keywordLen;
-    uint32_t rows = inputLen / columns;
 
+    
     //Main decryption Loop
     for(uint8_t i = 0; i < columns; i++){
+        //Calculate how many columns have been shorter before,
+        //to subtract them later from the index
+        uint8_t shorterColumns;
+        if(alphabetOrder[i] > inputLen % columns)
+            shorterColumns = alphabetOrder[i] - inputLen % columns + 1;
+        else
+            shorterColumns = 0;
+
         for(uint32_t j = 0; j < rows; j++){
-            dest[j * columns + i] = input[alphabetOrder[i] * rows + j];
+            if(j * columns + i >= inputLen)
+                continue;
+            dest[j * columns + i] = input[alphabetOrder[i] * rows + j - shorterColumns];
         }
     }
-    dest[inputLen] = 0;
+    dest[inputLen] = '\0';
 
     free(alphabetOrder);
 }
